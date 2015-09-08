6549bb41c02ab417d8cb905d71eb68e398458073
Adriano Schmidt <adriano@localhost8080.com.br>
2015-09-08 00:29:30 -0300

    renaming xml

--- app/src/main/java/br/com/localhost8080/findyourbus/activity/BusStopActivity.java
+++ app/src/main/java/br/com/localhost8080/findyourbus/activity/BusStopActivity.java
@@ -21,7 +21,7 @@ public class BusStopActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bus_stop_list);
+        setContentView(R.layout.activity_bus_stop);
 
         initializeBusDescription();
         initializeStopListView();
